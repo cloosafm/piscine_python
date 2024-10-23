74263fc08c36f6dceb53be0ed0ce40f981be7325
cloosafm <alexandra.cloos@gmail.com>
2024-10-23 14:33:12 +0200

    ex0

---
+++ 0_starting/Hello.py
@@ -0,0 +1,35 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+# ordered : elements have a defined order, and that order will not change.
+# unordered : order of use/apparition of elements is not guaranteed.
+# mutable : can be modified after creation.
+# immutable : cannot be modified after creation. have to declare a new one if modification needed.
+
+
+# lists are ordered, mutable. They can contain duplicates
+# defined with		[]
+ft_list.pop(1)
+ft_list.append("World!")
+
+# tuples are ordered, immutable. They can contain duplicates
+# defined with		()
+ft_tuple = ("Hello", "France!")
+
+# sets are unordered, mutable. They CANNOT contain duplicates
+# defined with		{}
+ft_set.discard("tutu!")
+ft_set.add("Paris!")
+
+# dictionaries are unordered, mutable (both keys and values). The keys must be unique, the values can be duplicated
+# defined with		{}		key-value pairs are separated by a colon :
+ft_dict["Hello"] = "42Paris!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
+
