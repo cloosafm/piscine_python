42272e18b0f340f131748a620ea5d7265df5c815
cloosafm <alexandra.cloos@gmail.com>
2024-10-24 14:50:47 +0200

    ex02 ok

--- 0_starting/ex02/find_ft_type.py
+++ 0_starting/ex02/find_ft_type.py
@@ -1,2 +1,18 @@
 def all_thing_is_obj(object: any) -> int:
-#your code here
+    object_name = object.__class__.__name__
+
+    match object_name:
+        case 'list':
+            print("List :", object.__class__)
+        case 'tuple':
+            print("Tuple :", object.__class__)
+        case 'set':
+            print("Set :", object.__class__)
+        case 'dict':
+            print("Dict :", object.__class__)
+        case 'str':
+            print(object, "is in the kitchen : ", object.__class__)
+        case _:
+            print("Type not found")
+
+    return 42
