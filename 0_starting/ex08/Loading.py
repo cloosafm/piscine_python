6f5361c6a30a05e6bd471e23dbf86b3dbbbf1fe1
cloosafm <alexandra.cloos@gmail.com>
2024-10-28 12:24:50 +0100

    seems ok, + norm + doc  ---- BUT somehow need to set bar length to 96?

--- 0_starting/ex08/Loading.py
+++ 0_starting/ex08/Loading.py
@@ -1,7 +1,14 @@
-for print statement:
+def ft_tqdm(lst: range) -> None:
+    """Decorate an iterable object, returning an iterator which acts exactly
+    like the original iterable, but prints a dynamically updating
+    progressbar every time a value is requested."""
 
-
-	start with \r to avoid using multiple lines
-	and end it with end=''
-
-print("\r",  blablabalbalbalbalab  ,  end='')
+    total = len(lst)
+    bar_length = 96
+    for i, item in enumerate(lst):
+        percent_complete = (i + 1) / total * 100
+        num_bars = int(percent_complete * (bar_length / 100))
+        bar = '|' + '█' * num_bars + ' ' * (bar_length - num_bars) + '| '
+        print(f"\r{percent_complete:3.0f}%{bar}{i + 1}/{total}", end='')
+        yield item
+    print()
