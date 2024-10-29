d0f381012389d996acf3f84adf2b62f688c10b5f
cloosafm <alexandra.cloos@gmail.com>
2024-10-29 10:32:52 +0100

    ex00 ok, norm, doc

--- 1_array/ex00/give_bmi.py
+++ 1_array/ex00/give_bmi.py
@@ -1,21 +1,38 @@
 import numpy as np
-# Allowed functions : numpy or any lib of table manipulation
 
 
-# BMI = weight (kg) / [height (m)]2
-# to compute BMI in metric system : multiply height by itself and divide weight by the result
-# for height = 180cm and weight = 78kg  => 78 / (1.8 * 1.8) = 24.074074074074073
-
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(height: list[int | float], weight: list[int | float]) \
+            -> list[int | float]:
     """
     Calculate the BMI for each person in the list.
+    BMI = weight / (height * height)
+    Units are kilograms and meters
     """
+    height_len = len(height)
+    weight_len = len(weight)
+    try:
+        if weight_len != height_len:
+            raise AssertionError("lists have different lengths")
+        for h, w in zip(height, weight):
+            if not isinstance(h, (int, float)) or h <= 0:
+                raise AssertionError("must be int or floats, and positive.")
+            if not isinstance(w, (int, float)) or w <= 0:
+                raise AssertionError("must be int or floats, and positive.")
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    return [w / (h * h) for h, w in zip(height, weight)]
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """
     Check whether given BMI is above the limit ("True") or not ("False").
     """
-    print("yo")
+    bmi_array = np.array(bmi)
+    result = []
 
-
+    for value in np.nditer(bmi_array):
+        if value < limit:
+            result.append(False)
+        else:
+            result.append(True)
+    return result
