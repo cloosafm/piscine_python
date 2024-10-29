998fdcb62b04a9336e586d3eb9efd506109f754e
cloosafm <alexandra.cloos@gmail.com>
2024-10-29 11:36:11 +0100

    ok for 01, incl. error check, norm, doc

--- 1_array/ex01/array2D.py
+++ 1_array/ex01/array2D.py
@@ -1,7 +1,34 @@
 import numpy
 
 
+def check_args(family: list, start: int, end: int):
+    """Check the args given for the slice_me fct"""
+    if not isinstance(family, list):
+        raise AssertionError("need a list as 1st arg")
+    if not isinstance(start | end, int):
+        raise AssertionError("need an int for start | end")
+
+    # Adjust for negative indices
+    if start < 0:
+        start += len(family)
+    if end < 0:
+        end += len(family)
+    if start < 0 or start >= len(family):
+        raise AssertionError("start position is outside list size")
+    if end < 0 or end > len(family):
+        raise AssertionError("end position is outside list size")
+    if start >= end:
+        raise AssertionError("end position is lower than start position")
+
+
 def slice_me(family: list, start: int, end: int) -> list:
+    """Take a list, a start and end position in this list
+    Return the list sliced according to given positions"""
+    try:
+        check_args(family, start, end)
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+        return None
     fam_array = numpy.array(family)
     print(f"My shape is : {fam_array.shape}")
     sliced_fam = fam_array[start:end]
--- 1_array/ex01/tester.py
+++ 1_array/ex01/tester.py
@@ -7,15 +7,3 @@ family = [[1.80, 78.4],
           [1.88, 75.2]]
 print(slice_me(family, 0, 2))
 print(slice_me(family, 1, -2))
-
-# Expected output:
-# $> python test_array2D.py
-
-# My shape is : (4, 2)
-# My new shape is : (2, 2)
-# [[1.8, 78.4], [2.15, 102.7]]
-
-# My shape is : (4, 2)
-# My new shape is : (1, 2)
-# [[2.15, 102.7]]
-# $>
