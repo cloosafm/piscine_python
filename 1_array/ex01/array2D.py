a6f6d47f24b1611a83110e273f47dbba9116eeb9
cloosafm <alexandra.cloos@gmail.com>
2024-10-29 10:55:24 +0100

    basic code ok for 01, need error case, norm, doc

--- 1_array/ex01/array2D.py
+++ 1_array/ex01/array2D.py
@@ -1,5 +1,11 @@
-Allowed functions : numpy or any lib of table manipulation
+import numpy
 
 
 def slice_me(family: list, start: int, end: int) -> list:
-#your code here
+    fam_array = numpy.array(family)
+    print(f"My shape is : {fam_array.shape}")
+    sliced_fam = fam_array[start:end]
+    print(f"My new shape is : {sliced_fam.shape}")
+    # need to revert to a list because of output format of arrays
+    family = sliced_fam.tolist()
+    return family
