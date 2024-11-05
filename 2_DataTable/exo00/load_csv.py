f7bfd16b99742bf6f068376f6fcd48380599aa2b
cloosafm <alexandra.cloos@gmail.com>
2024-11-05 12:04:10 +0100

    ex00 ook + norm + doc

--- 2_DataTable/exo00/load_csv.py
+++ 2_DataTable/exo00/load_csv.py
@@ -1,6 +1,21 @@
-from pandas import DataFrame as Dataset, read_csv
+from pandas import DataFrame as Dataset, read_csv, errors
 
 
 def load(path: str) -> Dataset:
-    data_set = read_csv(path)
-    return data_set
+    """
+    Returns dataset from file given in path
+    """
+    try:
+        data_set = read_csv(path)
+    except FileNotFoundError:
+        print(f"Error: The file at path '{path}' does not exist.")
+        return None
+    except errors.EmptyDataError:
+        print(f"Error: The file at path '{path}' is empty.")
+        return None
+    except errors.ParserError:
+        print(f"Error: The file at path '{path}' could not be parsed.")
+        return None
+    else:
+        print(f"Loading dataset of dimensions {data_set.shape}")
+        return data_set
