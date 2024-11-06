845ae1aca347270d065974f87d587424a11963a5
cloosafm <alexandra.cloos@gmail.com>
2024-11-06 12:07:53 +0100

    02/ex01 ok + norm + doc

--- 2_DataTable/exo01/aff_life.py
+++ 2_DataTable/exo01/aff_life.py
@@ -2,22 +2,48 @@ from load_csv import load
 from matplotlib import pyplot as plt
 
 
+def plot_country_data(df, country: str):
+    """
+    Plot the data for a specific country.
+
+    Args:
+        df : The DataFrame containing the data.
+        country (str): The name of the country to plot.
+    """
+    if df is not None:
+        # Transpose the DataFrame
+        df_transposed = df.T
+        # Set the first row as the header using loc
+        df_transposed.columns = df_transposed.loc[df_transposed.index[0]]
+        df_transposed = df_transposed.loc[df_transposed.index[1:]]
+        # Set index to years, convert to int
+        df_transposed.index = df_transposed.index.astype(int)
+        # Check if the specified country exists in the DataFrame
+        if country not in df_transposed.columns:
+            print(f"Error: The country '{country}' does not exist.")
+            return
+
+        # Plot the data for the specified country
+        plt.plot(df_transposed.index, df_transposed[country])
+        # Customize ticks on the x-axis
+        min_year = df_transposed.index.min()
+        max_year = df_transposed.index.max()
+        plt.xticks(range(min_year, max_year, 40))
+        # Add labels and title
+        plt.xlabel('Year')
+        plt.ylabel('Life expectancy')
+        plt.title(f"{country} Life expectancy Projections")
+
+        plt.show()
+
+
 def main():
     """
     Loads a csv file and displays its info about France
     """
-    life_data = load("life_expectancy_years.csv")
-    if life_data is not None:
-        life_data = life_data.T
-        life_data.columns = life_data.loc['France']
-        # life_data = life_data[1:]
-        print(life_data)
-        plt.plot(life_data.loc["France"])
-
-        # plt.xlabel('Category')
-        # plt.ylabel('Values')
-        # plt.title('Bar Chart from CSV')
-        # plt.show()
+    df = load("life_expectancy_years.csv")
+    if df is not None:
+        plot_country_data(df, "France")
 
 
 if __name__ == "__main__":
