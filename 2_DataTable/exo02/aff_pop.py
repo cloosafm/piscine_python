34456040d8ba50cfc6aec1a212d3486c4da16712
cloosafm <alexandra.cloos@gmail.com>
2024-11-06 15:34:31 +0100

    need to change y-axis ticks

--- 2_DataTable/exo02/aff_pop.py
+++ 2_DataTable/exo02/aff_pop.py
@@ -49,23 +49,40 @@ def plot_country_data(df, country1: str, country2: str) -> None:
         df_transposed[country1] = df_transposed[country1].apply(convert_population_to_float)
         df_transposed[country2] = df_transposed[country2].apply(convert_population_to_float)
 
-        # Plot the data for the specified country
-        plt.plot(df_transposed.index, df_transposed[country1],
-                 label=country1, color='b')
-        plt.plot(df_transposed.index, df_transposed[country2],
-                 label=country2, color='g')
+        # # Plot the data for the specified country
+        # plt.plot(df_transposed.index, df_transposed[country1],
+        #          label=country1, color='b')
+        # plt.plot(df_transposed.index, df_transposed[country2],
+        #          label=country2, color='g')
+
+        # # Customize ticks on the x-axis
+        # min_year = df_transposed.index.min()
+        # max_year = df_transposed.index.max()
+        # plt.xticks(range(min_year, max_year, 40))
+        # # # Customize ticks on the y-axis
+        # formatter = ticker.EngFormatter(unit='M')
+        # min_pop = int(min(df_transposed[country1].min(), df_transposed[country2].min()))
+        # max_pop = int(max(df_transposed[country1].max(), df_transposed[country2].max()))
+        # plt.yticks(range(min_pop, max_pop, 20_000_000))
+
+# Plot the data for the specified countries
+        fig, ax = plt.subplots()
+        ax.plot(df_transposed.index, df_transposed[country1], label=country1, color='b')
+        ax.plot(df_transposed.index, df_transposed[country2], label=country2, color='g')
 
         # Customize ticks on the x-axis
         min_year = df_transposed.index.min()
         max_year = df_transposed.index.max()
-        plt.xticks(range(min_year, max_year, 40))
-        # # Customize ticks on the y-axis
-        formatter = ticker.EngFormatter(unit='M')
+        ax.set_xticks(range(min_year, max_year + 1, 40))
+        
+        # Customize ticks on the y-axis to be every 20 million
         min_pop = int(min(df_transposed[country1].min(), df_transposed[country2].min()))
         max_pop = int(max(df_transposed[country1].max(), df_transposed[country2].max()))
-        plt.yticks(range(min_pop, max_pop, 20_000_000))
-
-
+        ax.set_yticks(range(min_pop, max_pop + 1, 20_000_000))
+        
+        # Apply the custom formatter to the y-axis
+        formatter = ticker.EngFormatter(unit='M')
+        ax.yaxis.set_major_formatter(formatter)
 
         # Add labels, legend and title
         plt.xlabel('Year')
