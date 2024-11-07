8a281d8fc61dffba1eae9087cda85698f3339473
cloosafm <alexandra.cloos@gmail.com>
2024-11-07 15:11:43 +0100

    2/ex03 almost done, need to add start point on x-scale

--- 2_DataTable/exo03/projection_life.py
+++ 2_DataTable/exo03/projection_life.py
@@ -19,6 +19,17 @@ def plot_projection_life(df_gdp, df_life, year: int):
     # Transpose the DataFrames
     df_gdp = df_gdp.T
     df_life = df_life.T
+
+
+    # Check if the specified year exists in the DataFrames
+    # if year not in df_gdp.index:
+    #     print(f"Error: The year {year} does not exist in gdp.")
+    #     return
+    # if year not in df_life.index:
+    #     print(f"Error: The year {year} does not exist in life.")
+    #     return
+
+
     # Set the first row of each Dataframe as the header using loc
     df_gdp.columns = df_gdp.loc[df_gdp.index[0]]
     df_gdp = df_gdp.loc[df_gdp.index[1:]]
@@ -28,7 +39,37 @@ def plot_projection_life(df_gdp, df_life, year: int):
     df_gdp.index = df_gdp.index.astype(int)
     df_life.index = df_life.index.astype(int)
 
-# plot type : scatter
+    # plot type : scatter
+    fig, ax = plt.subplots()
+    ax.scatter(df_gdp.loc[year], df_life.loc[year])
+
+    # Customize the x-axis to be logarithmic
+    ax.set_xscale('log')
+
+    # Apply the custom formatter to the x-axis
+    formatter = ticker.EngFormatter(sep="")
+    ax.xaxis.set_major_formatter(formatter)
+
+    logfmt = ticker.LogFormatterSciNotation()
+    ax.xaxis.set_minor_formatter(logfmt, minor_thresholds=)
+
+    # Calculate the minimum value in the x-axis data
+    min_gdp = df_gdp.loc[year].min()
+
+    # Determine the lowest hundred value less than or equal to the minimum value
+    start_value = (min_gdp // 100) * 100
+
+    # Set x-axis limits to start at the calculated value
+    ax.set_xlim(left=start_value)
+
+
+    # Add labels, legend and title
+    ax.set_xlabel('Gross domestic product')
+    ax.set_ylabel('Life Expectancy')
+    ax.set_title(f"{year}")
+
+    plt.show()
+
 
 def main():
     """
@@ -40,6 +81,20 @@ def main():
 
     if df_gdp is not None and df_life is not None:
         year = 1900
+        # Ensure same number of rows (countries) in both DataFrames
+        if df_gdp.shape[0] != df_life.shape[0]:
+            print("Warning: The DataFrames have a different number of rows.")
+            return
+        # Check if the specified year exists in the DataFrames
+        if str(year) not in df_gdp.columns and str(year) not in df_life.columns:
+            print(f"Error: The year {year} is inexistent in both DataFrames.")
+            return
+        elif str(year) not in df_gdp.columns:
+            print(f"Error: The year {year} is inexistent in the GDP DataFrame.")
+            return
+        elif str(year) not in df_life.columns:
+            print(f"Error: The year {year} is inexistent in the Life Expectancy DataFrame.")
+            return
         plot_projection_life(df_gdp, df_life, year)
 
 
