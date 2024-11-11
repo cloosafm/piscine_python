cb752cc033cf1ec9ff53d5d9f85298014a53e89d
cloosafm <alexandra.cloos@gmail.com>
2024-11-11 16:02:39 +0100

    updated 2/02 with convert class, working on widgeting

--- 2_DataTable/exo03/projection_life.py
+++ 2_DataTable/exo03/projection_life.py
@@ -1,56 +1,34 @@
 from load_csv import load
-from matplotlib import pyplot as plt, ticker
-import mplcursors
+from matplotlib import pyplot as plt, ticker, widgets
 import numpy as np
+from pint import UnitRegistry
 
 
-def str_to_float(value):
-    """
-    Convert a string to a float.
-    
-    Args:
-        value (str): The string to convert.
-    
-    Returns:
-        float: The converted float value.
-    """
-    if isinstance(value, str):
-        if value.endswith('M'):
-            return float(value[:-1]) * 1e6
-        elif value.endswith('K'):
-            return float(value[:-1]) * 1e3
-    return float(value)
+class Convert:
+    """Class for converting strs with units to actual numbers"""
+    def __init__(self):
+        """Constructor for the Convert class"""
+        self.ureg = UnitRegistry()
+        self.ureg.define("thousand = 1e3 = k")
+        self.ureg.define("million = 1e6 = M")
+        self.ureg.define("billion = 1e9 = B")
 
-def handleEvent(event, df_gdp, df_life, year, countries):
-    """
-    Handle the event when a point in the scatter plot is clicked.
-    
-    Args:
-        event: The event object.
-        df_gdp (pd.DataFrame): The GDP DataFrame.
-        df_life (pd.DataFrame): The life expectancy DataFrame.
-        year (int): The year for the projection.
-        countries (list): The list of country names.
-    """
-    if event.inaxes:
-        # Get the x and y data from the event
-        xdata = event.xdata
-        ydata = event.ydata
-        print(f"Clicked on point: ({xdata}, {ydata})")
-        
-        # Find the closest point in the scatter plot
-        gdp_values = np.array(df_gdp.loc[year].values)
-        life_values = np.array(df_life.loc[year].values)
-        distances = np.sqrt((gdp_values - xdata)**2 + (life_values - ydata)**2)
-        min_index = np.argmin(distances)
-        
-        # Print the country name, GDP, and life expectancy
-        country = countries[min_index]
-        gdp = gdp_values[min_index]
-        life_expectancy = life_values[min_index]
-        print(f"Country: {country}, GDP: {gdp}, Life Expectancy: {life_expectancy}")
+    def convert_to_float(self, value: str) -> float:
+        """
+        Convert a string with units to a float.
+        Args:
+            value (str): The string with units (e.g., '3.28M').
+        Returns:
+            float: The value as a float.
+        """
+        try:
+            return float(value)
+        except ValueError:
+            return self.ureg(value).to_base_units().magnitude
 
-def plot_projection_life(df_init_gdp, df_init_life, year: int):
+
+
+def plot_projection_life(df_gdp, df_life, year: int):
     """
     Plots the projection of life expectancy in relation to the GDP.
     Args:
@@ -62,17 +40,9 @@ def plot_projection_life(df_init_gdp, df_init_life, year: int):
         # remove it from the other csv file?
         # print a warning message?
 
-
-    # Apply the str_to_float function to all columns except the first column
-    df_gdp = df_init_gdp.copy()
-    df_life = df_init_life.copy()
-    df_gdp.iloc[:, 1:] = df_gdp.iloc[:, 1:].applymap(str_to_float)
-    df_life.iloc[:, 1:] = df_life.iloc[:, 1:].applymap(str_to_float)
-
     # Transpose the DataFrames
     df_gdp = df_gdp.T
     df_life = df_life.T
-
 
     # Set the first row of each Dataframe as the header using loc
     df_gdp.columns = df_gdp.loc[df_gdp.index[0]]
@@ -80,16 +50,25 @@ def plot_projection_life(df_init_gdp, df_init_life, year: int):
     df_life.columns = df_life.loc[df_life.index[0]]
     df_life = df_life.loc[df_life.index[1:]]
 
+    # Convert GDP values to float
+    convert = Convert()
+    df_gdp = df_gdp.map(convert.convert_to_float)
 
+    # Align the DataFrames based on their common columns and indexes
+    common_countries = df_gdp.columns.intersection(df_life.columns)
+    df_gdp = df_gdp[common_countries]
+    df_life = df_life[common_countries]
 
+    common_years = df_gdp.index.intersection(df_life.index)
+    df_gdp = df_gdp.loc[common_years]
+    df_life = df_life.loc[common_years]
 
 
     # Set indexes to years, convert to float
     df_gdp.index = df_gdp.index.astype(float)
     df_life.index = df_life.index.astype(float)
 
-    # Get the list of countries
-    countries = df_gdp.columns.tolist()
+
 
 
     # plot type : scatter
@@ -117,31 +96,29 @@ def plot_projection_life(df_init_gdp, df_init_life, year: int):
     ax.xaxis.set_major_formatter(formatter)
 
 
-    # fig.canvas.mpl_connect('button_press_event', lambda event: handleEvent(event, df_gdp, df_life, year, countries))
-
-
-    cursor = mplcursors.cursor(
-        plt.scatter(df_gdp.astype(float), df_life.astype(float))
-    )
-    cursor.connect(
-        "add",
-        lambda sel: sel.annotation.set_text(
-            "Country: {}\nGDP: {}\nLife expectancy: {}".format(
-                df_gdp["country"][sel.index], sel.target[0], sel.target[1]
-            )
-        ),
-    )
-
-
-
 
     # Add labels, legend and title
     ax.set_xlabel('Gross domestic product')
     ax.set_ylabel('Life Expectancy')
     ax.set_title(f"{year}")
 
+
+    # cursor = mplcursors.cursor(
+    #     plt.scatter(df_gdp.astype(float), df_life.astype(float))
+    # )
+    # cursor = widgets.Cursor(ax, useblit=True, color='red', linewidth=2)
+    # cursor.connect(
+    #     "add",
+    #     lambda sel: sel.annotation.set_text(
+    #         "Country: {}\nGDP: {}\nLife expectancy: {}".format(
+    #             df_gdp["country"][sel.index], sel.target[0], sel.target[1]
+    #         )
+    #     ),
+    # )
+
+    cursor = widgets.Cursor(ax, useblit=True, color='red', linewidth=2)
     plt.show()
-    # plt.ion()
+
 
 
 def main():
