f2a0022c5ddb0f5f1cae9b40b393357e369639d8
cloosafm <alexandra.cloos@gmail.com>
2024-11-12 12:10:45 +0100

    3/ex00 pretty much ok, need more errro mngmt, norm + doc ok

--- 3_OOP/ex00/S1E9.py
+++ 3_OOP/ex00/S1E9.py
@@ -6,24 +6,33 @@ class Character(ABC):
         Abstract class
         Args:
             first_name (str): The first name of the character.
-    		is_alive (bool): The status of the character.
+            is_alive (bool): The status of the character.
                 optional, defaults to True.
     """
-    # @abstractmethod
-    #your code here
-    def __init__(self, first_name, is_alive=True):
+
+# add error magmt, checks for attribute types
+    def __init__(self, first_name: str, is_alive: bool = True):
         '''
-        Initializes character with first name and optional is_alive status.
+        Constructor.
+        Initialize character with following args:
+            - first name
+            - (optional) is_alive status.
         '''
         self.first_name = first_name
         self.is_alive = is_alive
 
+    @abstractmethod
     def die(self):
         '''
         Kills the character.
         '''
-        self.is_alive = False
+        pass
+
 
 class Stark(Character):
     """Inherit from abstract Character class"""
-    #your code here
+    def die(self):
+        '''
+        Kills the Stark character.
+        '''
+        self.is_alive = False
