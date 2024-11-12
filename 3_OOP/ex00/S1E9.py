d7b21415fb776a91887a14e67375e38ad8ad20a5
cloosafm <alexandra.cloos@gmail.com>
2024-11-12 10:41:12 +0100

    got basic inheritance in place, now need to make Character class be abstract

--- 3_OOP/ex00/S1E9.py
+++ 3_OOP/ex00/S1E9.py
@@ -2,16 +2,28 @@ from abc import ABC, abstractmethod
 
 
 class Character(ABC):
-"""
-    Abstract class
-    Args:
-        first_name (str): The first name of the character.
-		is_alive (bool): The status of the character.
-            optional, defaults to True.
-"""
-@abstractmethod
-#your code here
+    """
+        Abstract class
+        Args:
+            first_name (str): The first name of the character.
+    		is_alive (bool): The status of the character.
+                optional, defaults to True.
+    """
+    # @abstractmethod
+    #your code here
+    def __init__(self, first_name, is_alive=True):
+        '''
+        Initializes character with first name and optional is_alive status.
+        '''
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    def die(self):
+        '''
+        Kills the character.
+        '''
+        self.is_alive = False
 
 class Stark(Character):
-"""Inherit from abstract Character class"""
-#your code here
+    """Inherit from abstract Character class"""
+    #your code here
