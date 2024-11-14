b3727d40dc23082b2ac726e76bb4a11977b81685
cloosafm <alexandra.cloos@gmail.com>
2024-11-14 14:30:19 +0100

    4/01 ok + doc + norm

--- 4_Dod/ex01/in_out.py
+++ 4_Dod/ex01/in_out.py
@@ -1,8 +1,44 @@
 def square(x: int | float) -> int | float:
-#your code here
+    """Return the square of the input.
+        Args:
+            x: int or float
+        Returns:
+            int or float
+    """
+    if not isinstance(x, (int, float)):
+        raise TypeError("Input must be an integer or a float.")
+    return x * x
+
+
 def pow(x: int | float) -> int | float:
-#your code here
+    """Return the exponentiation of the input.
+        Args:
+            x: int or float
+        Returns:
+            int or float
+    """
+    if not isinstance(x, (int, float)):
+        raise TypeError("Input must be an integer or a float.")
+    return x ** x
+
+
 def outer(x: int | float, function) -> object:
-count = 0
-def inner() -> float:
-#your code here
+    """
+    Function that takes as argument a number and a function
+    Return an object that will return the result of the arguments calculation.
+    Args:
+        x: int or float
+        function: a function
+    Returns:
+        an object
+    """
+    count = 0
+
+    def inner() -> float:
+        nonlocal count
+        res = function(x)
+        for i in range(count):
+            res = function(res)
+        count += 1
+        return res
+    return inner
