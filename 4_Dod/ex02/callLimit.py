98ce54744a86b046d932368ee9700b2383199859
cloosafm <alexandra.cloos@gmail.com>
2024-11-14 16:14:49 +0100

    4/02 ok + norm +doc

--- 4_Dod/ex02/callLimit.py
+++ 4_Dod/ex02/callLimit.py
@@ -1,5 +1,36 @@
 def callLimit(limit: int):
-	count = 0
-	def callLimiter(function):
-		def limit_function(*args: Any, **kwds: Any):
-		#your code here
+    """
+    Decorator that limits the number of times a function can be called.
+    Args:
+        limit: int: The limit of the function.
+    Returns:
+        function: The wrapped function.
+    """
+    count = 0
+
+    def callLimiter(function):
+        """
+        Wrapper function.
+        Call the limiting function.
+        Args:
+            function: function: The function to limit.
+        Returns:
+            function: The called function, if within limits.
+        """
+        def limit_function(*args: any, **kwds: any):
+            """
+            Function that returns a given function, within limits.
+            If function was called too many times, prints error message.
+            Args:
+                *args: any: Any number of arguments.
+                **kwds: any: Any number of keyword arguments.
+            Returns:
+                any: The result of the function call.
+            """
+            nonlocal count
+            if count < limit:
+                count += 1
+                return function(*args, **kwds)
+            print(f"Error: {function} call too many times")
+        return limit_function
+    return callLimiter
